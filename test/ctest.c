9b6de22105bf3e5aac07ac2452cbdee91baad07d
Пастухов Кирилл <kirill.pastukhov.90@mail.ru>
2023-08-28 22:02:24 +0700

    Изменён ctest.c

--- test/ctest.c
+++ test/ctest.c
@@ -7,11 +7,20 @@ using namespace std;
 
 CTEST(Functions, SbrosVvoda)
 {
- char simvol = ' ';
- cin >> simvol;
- SbrosVvoda();
- if (cin.peek() == '\n')
-	 ASSERT_EQUAL(1,1);
- else 
-	 ASSERT_EQUAL(1,0);
+    string input = "5";
+    stringstream iss(input);
+    cin.rdbuf(iss.rdbuf());
+    char simvol = ' ';
+    cin >> simvol;
+    SbrosVvoda();
+    if (cin.peek() == '\n')
+        ASSERT_EQUAL(1, 1);
+    else
+        ASSERT_EQUAL(1, 0);
+}
+
+int main(int argc, const char** argv)
+{
+    int test_result = ctest_main(argc, argv);
+    return test_result;
 }
