2b6933039f65b7b807dab90907a1e177ca6565d2
Пастухов Кирилл <kirill.pastukhov.90@mail.ru>
2023-09-29 12:00:33 +0700

    Добавлены тесты

--- test/ctest.c
+++ test/ctest.c
@@ -1,22 +1,22 @@
-// #include <sstream>
-// #include <iostream>
-// #include <string>
+ #include <sstream>
+ #include <iostream>
+ #include <string>
 #include <ctest.h>
-// #include "../src/libapp/sbros.hpp"
+ #include "../src/libapp/sbros.hpp"
 
 CTEST(Functions, SbrosVvoda)
 {
     ASSERT_EQUAL(1, 1);
-    // string input = "5";
-    // stringstream iss(input); 
-    // cin.rdbuf(iss.rdbuf());
-    // char simvol = ' ';
-    // cin >> simvol;
-    // SbrosVvoda();
-    // if (cin.peek() == '\n'){
-    //     ASSERT_EQUAL(1, 1);
-    //     }
-    // else{
-    //     ASSERT_EQUAL(1, 0);
-    //     }
+     string input = "5";
+     stringstream iss(input); 
+     cin.rdbuf(iss.rdbuf());
+     char simvol = ' ';
+     cin >> simvol;
+     SbrosVvoda();
+     if (cin.peek() == '\n'){
+         ASSERT_EQUAL(1, 1);
+         }
+     else{
+         ASSERT_EQUAL(1, 0);
+         }
 }
